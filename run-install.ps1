7191b492878e02df639c120054dd1e3308f94389
jengryg <jengryg@users.noreply.github.com>
2024-08-13 01:02:49 +0200

    Add run-install.ps1 script as main entry point.

---
+++ run-install.ps1
@@ -0,0 +1,39 @@
+<#
+    .SYNOPSIS
+    Runs the installation process.
+
+    .DESCRIPTION
+    This script is the main entry point for the setup process.
+    Running this script will orchestrate the setup step by step and configure the system.
+
+    .PARAMETER DeleteBeforeInstall
+    If this switch is enabled, the setup will delete directories containing configurations and also unregister the
+    Ubuntu distribution without making any backups. This switch is only meant to be used when developing/debugging
+    this setup repository.
+#>
+param(
+    [Switch]$DeleteBeforeInstall = $false
+)
+
+# To avoid issues with the ~ shortcut, we obtain the users home directory path and store it for later use.
+$windowsUserHome = Resolve-Path ~
+
+# By default, all scripts will work with the .dev-setup subdirectory in the $windowsUserHome directory.
+$setupPath = "$windowsUserHome\.dev-setup"
+
+if ($DeleteBeforeInstall)
+{
+    Remove-Item -Recurse -Force $setupPath
+    wsl --unregister Ubuntu-24.04
+}
+
+if (Test-Path $setupPath)
+{
+    throw "Directory $setupPath already exists. Aborting to prevent conflicts."
+}
+
+Write-Host "Creating $setupPath directory."
+[Void](New-Item -Path $setupPath -ItemType Directory)
+
+# initialzie the wsl with ubuntu and cloud init configuration.
+& $PSScriptRoot\wsl-cloud-init/setup-wsl-cloud-init-config.ps1
