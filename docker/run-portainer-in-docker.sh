820c4240a9dbb93f1bc7e98a7610e7dfaba74488
jengryg <jengryg@users.noreply.github.com>
2024-08-13 01:02:49 +0200

    Add portainer to the docker instance in wsl.

---
+++ docker/run-portainer-in-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# create the volume for the portainer database
+docker volume create portainer_data
+
+# download and install the portainer container with docker
+docker run -d -p 8000:8000 \
+              -p 9443:9443 \
+              --name portainer \
+              --restart=always \
+              -v /var/run/docker.sock:/var/run/docker.sock \
+              -v portainer_data:/data portainer/portainer-ce:latest
