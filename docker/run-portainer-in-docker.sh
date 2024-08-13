a094a96c759f58bc98acb4f5f1c98a45add50103
jengryg <jengryg@users.noreply.github.com>
2024-08-13 11:37:58 +0200

    Prevent linebreak issue in bash.

--- docker/run-portainer-in-docker.sh
+++ docker/run-portainer-in-docker.sh
@@ -9,4 +9,5 @@ docker run -d -p 8000:8000 \
               --name portainer \
               --restart=always \
               -v /var/run/docker.sock:/var/run/docker.sock \
-              -v portainer_data:/data portainer/portainer-ce:latest
+              -v portainer_data:/data \
+              portainer/portainer-ce:latest
