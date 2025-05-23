a4fcfe1849e3f33dd67a553d8d14c05972ee91c1
jengryg <62914315+jengryg@users.noreply.github.com>
2025-05-24 01:46:36 +0200

    Fixing Issues.
    kubectl now works again in wsl after install
    helm deployment now ensures that the repo is there and updated

--- k8s-deployments/signoz/default/helm-install-chart.sh
+++ k8s-deployments/signoz/default/helm-install-chart.sh
@@ -1,2 +1,9 @@
 helm repo add signoz https://charts.signoz.io
-helm -n signoz --create-namespace install "signoz-default" signoz/signoz
+helm repo update
+
+echo "Starting the installation of the helm chart. This window will wait until deployment is ready or timeout after 1h."
+echo "Installation can take several minutes where nothing is shown in the console.."
+
+helm install "signoz-default" signoz/signoz \
+    --namespace signoz --create-namespace \
+    --wait --timeout 1h
--- k8s-kind/expose-kind-on-windows.sh
+++ k8s-kind/expose-kind-on-windows.sh
@@ -6,5 +6,5 @@ kubePath="$windowsUserHome/.kube/"
 # ensure that the directory exists
 mkdir -p "$kubePath"
 
-# move the config to the windows user directory to make the cluster directly available in windows
-sudo mv ~/.kube/config "$kubePath"
+# copy the config to the windows user directory to make the cluster directly available in windows too
+sudo cp ~/.kube/config "$kubePath"
